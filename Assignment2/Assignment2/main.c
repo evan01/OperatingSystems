a9bf48cb44006c831e91826c2a2673c97576c71d
Evan Knox <eknoxmobile@gmail.com>
2016-02-22 10:50:50 -0500

    Commit of A2
    Committing A2! Actually not worth committing the Xcode files…

---
+++ Assignment2/Assignment2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Assignment2
+//
+//  Created by Evan Knox on 2016-02-22.
+//  Copyright © 2016 NaliApplications. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
